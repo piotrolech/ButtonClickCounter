bc73d234877cf9f52ada388766e130cc433ebe64
Piotr Olech <Piotr Olech>
2017-05-03 20:38:42 +0200

    Initial commit

---
+++ app/src/main/java/pl/piotrolech/a20170502buttonclickcounter/MainActivity.java
@@ -0,0 +1,108 @@
+package pl.piotrolech.a20170502buttonclickcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private TextView textView;
+    private int clicksNumber;
+    private static final String TAG = "MainActivity";
+    private static final String CONTENT_TEXT = "ContentText";
+    private static final String CONTENT_INT = "ContentInt";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userInput = (EditText) findViewById(R.id.editText);
+        userInput.setText("");
+        Button button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicksNumber++;
+                String result = userInput.getText().toString() + "\n";
+                String counter = "Counter: " + clicksNumber + "\n";
+                textView.append(result);
+                textView.append(counter);
+                userInput.setText("");
+            }
+        };
+        button.setOnClickListener(buttonClickListener);
+        Log.d(TAG, "onCreate: out");
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+        textView.setText(savedInstanceState.getString(CONTENT_TEXT));
+        clicksNumber = savedInstanceState.getInt(CONTENT_INT);
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: in");
+        super.onResume();
+        Log.d(TAG, "onResume: out");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: in");
+        super.onPause();
+        Log.d(TAG, "onPause: out");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        outState.putString(CONTENT_TEXT, textView.getText().toString());
+        outState.putInt(CONTENT_INT, clicksNumber);
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: in");
+        super.onStop();
+        Log.d(TAG, "onStop: out");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: in");
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: out");
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
+        super.onRestart();
+        Log.d(TAG, "onRestart: out");
+    }
+}
